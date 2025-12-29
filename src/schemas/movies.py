1ffd5a92dee792efffb251d32b47fc31989d50a7
Artur Bashyrov <bashyroov@gmail.com>
2025-12-29 21:46:30 +0100

    Added schemas for Movie model

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,29 @@
-# Write your code here
+from pydantic import BaseModel
+import datetime
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: int
+    date: datetime.date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+
+    class Config:
+        orm_mode = True
+
+
+class MovieListResponseSchema(BaseModel):
+    prev_page: int | None
+    next_page: int | None
+    total_pages: int
+    total_items: int
